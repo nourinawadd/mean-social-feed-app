c1ffb55a50a0ca69d903f54614f33655e6bde49d
nourinawadd <nourinawad@gmail.com>
2025-08-03 20:49:59 +0300

    add user routing and middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        if (!req.user) throw new Error();
+        next();
+    } catch (err) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+};
+
+module.exports = authMiddleware;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const postRoutes = require('./routes/post.routes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,5 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/', postRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
