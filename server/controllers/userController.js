912fc04de7ebca8493ae0d58fc78c78cb8e6c027
nourinawadd <nourinawad@gmail.com>
2025-08-03 20:43:48 +0300

    edit user schema and controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,23 +1,32 @@
-const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const generateToken = (user) => {
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
 
 exports.register = async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ username, email, password });
+        await user.save();
+        const token = generateToken(user);
+        res.json({ token, user: { id: user._id, username: user.username } });
+    } catch (err) {
+        res.status(400).json({ error: 'Registration failed', details: err.message });
+    }
 };
 
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user || !(await user.comparePassword(password))) {
-    return res.status(401).json({ error: 'Invalid credentials' });
-  }
-
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-  res.json({ token, user });
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+        const token = generateToken(user);
+        res.json({ token, user: { id: user._id, username: user.username } });
+    } catch (err) {
+        res.status(500).json({ error: 'Login failed' });
+    }
 };
